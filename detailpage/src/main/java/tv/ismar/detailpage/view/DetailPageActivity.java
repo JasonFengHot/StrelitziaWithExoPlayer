5b0a71ac0013e7bf9284d76a52d82423d86c725a
longhai <longhai326@163.com>
2016-08-19 10:03:39 +0800

    修改包结构

---
+++ detailpage/src/main/java/tv/ismar/detailpage/view/DetailPageActivity.java
@@ -0,0 +1,19 @@
+package tv.ismar.detailpage.view;
+
+import android.app.Activity;
+import android.view.View;
+
+import tv.ismar.detailpage.R;
+
+/**
+ * Created by huibin on 8/18/16.
+ */
+public class DetailPageActivity extends Activity {
+
+    @Override
+    public void setContentView(View view) {
+        setContentView(R.layout.activity_detailpage);
+    }
+
+
+}
---
+++ detailpage/src/main/java/tv/ismar/detailpage/viewmodel/ViewModel.java
@@ -0,0 +1,9 @@
+package tv.ismar.detailpage.viewmodel;
+
+/**
+ * Created by huibin on 8/18/16.
+ */
+public interface ViewModel {
+
+
+}
