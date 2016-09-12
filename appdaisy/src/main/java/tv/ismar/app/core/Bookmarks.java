4e54d665a55b6bf7d6c47abe180ecb80d46a5eea
fenghuibin <fenghuibin@ismartv.cn>
2016-09-12 16:39:08 +0800

    bookmarks moduleVersion = '1.22'

---
+++ appdaisy/src/main/java/tv/ismar/app/core/Bookmarks.java
@@ -0,0 +1,16 @@
+package tv.ismar.app.core;
+
+/**
+ * Created by huibin on 9/12/16.
+ */
+public class Bookmarks implements BookmarksInterface{
+    @Override
+    public void addBookmarks() {
+
+    }
+
+    @Override
+    public void removeBookmarks() {
+
+    }
+}
--- appdaisy/src/main/java/tv/ismar/app/core/BookmarksInterface.java
+++ appdaisy/src/main/java/tv/ismar/app/core/BookmarksInterface.java
@@ -1,11 +1,13 @@
 package tv.ismar.app.core;
 
+import android.content.Context;
+
 /**
  * Created by huibin on 8/24/16.
  */
 public interface BookmarksInterface {
-    void addBookmarks();
+    void addBookmarks(Context context, Object item);
 
-    void removeBookmarks();
+    void removeBookmarks(Context context, Object item);
 
 }
