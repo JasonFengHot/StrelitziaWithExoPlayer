4a840d69143087a0a225081ccb71ec3b3ad3a0f2
fenghuibin <fenghuibin@ismartv.cn>
2016-09-13 18:12:32 +0800

    modify ui

--- ismartvpay/src/main/java/tv/ismar/pay/PaymentActivity.java
+++ ismartvpay/src/main/java/tv/ismar/pay/PaymentActivity.java
@@ -1,7 +1,6 @@
 package tv.ismar.pay;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import tv.ismar.app.BaseActivity;
 
@@ -9,8 +8,10 @@ import tv.ismar.app.BaseActivity;
  * Created by huibin on 9/13/16.
  */
 public class PaymentActivity extends BaseActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
     }
 }
