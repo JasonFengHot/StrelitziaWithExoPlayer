1b05179d41cf581b1e89c156d65c20acba82f3a4
fenghuibin <fenghuibin@ismartv.cn>
2016-09-22 17:18:40 +0800

    moduleVersion = '1.50'

--- bintrayUpload.sh
+++ bintrayUpload.sh
@@ -3,6 +3,7 @@ gradle -q -b detailpage/build.gradle bintrayUpload
 gradle -q -b account/build.gradle bintrayUpload
 gradle -q -b ismartvpay/build.gradle bintrayUpload
 gradle -q -b appdaisy/build.gradle bintrayUpload
+gradle -q -b playerpage/build.gradle bintrayUpload
 
 
 
