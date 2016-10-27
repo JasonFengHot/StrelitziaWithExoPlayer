50ab406bd68ce485f08008b18c05a9c431404f79
longhai <longhai@ismartv.cn>
2016-10-27 15:20:20 +0800

    开机广告下载优化

--- appbase/src/main/java/tv/ismar/app/ad/AdsUpdateService.java
+++ appbase/src/main/java/tv/ismar/app/ad/AdsUpdateService.java
@@ -6,15 +6,19 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 
-import com.google.gson.Gson;
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import cn.ismartv.injectdb.library.query.Delete;
+import cn.ismartv.injectdb.library.query.Select;
+import tv.ismar.app.db.AdvertiseTable;
 import tv.ismar.app.network.entity.AdElementEntity;
-import tv.ismar.app.util.SPUtils;
-import tv.ismar.app.util.Utils;
+import tv.ismar.app.util.FileUtils;
 
 public class AdsUpdateService extends Service implements Advertisement.OnAppStartAdListener {
 
@@ -55,14 +59,44 @@ public class AdsUpdateService extends Service implements Advertisement.OnAppStar
     @Override
     public void loadAppStartAd(List<AdElementEntity> adList) {
         if (adList != null && !adList.isEmpty()) {
-            String shared = new Gson().toJson(adList).replaceAll(" ", "");
-            String savedJson = (String) SPUtils.getValue(AdvertiseManager.EXTRA_LAUNCH_APP_AD, "");
-            if (!Utils.isEmptyText(savedJson) && shared.equals(savedJson)) {
-                return;
+            List<AdElementEntity> needDownloadAds = new ArrayList<>();
+            List<AdvertiseTable> advertisementTables = new Select().from(AdvertiseTable.class).execute();
+            if (advertisementTables == null || advertisementTables.isEmpty()) {
+                needDownloadAds.addAll(adList);
+            } else {
+                // 数据库中没有的数据需要下载,数据库中有服务器没有的需要删除,都有的不下载
+                Map<String, AdElementEntity> adMaps = new HashMap<>();
+                for (AdElementEntity adEntity : adList) {
+                    adMaps.put(adEntity.getMd5(), adEntity);
+                }
+                for (AdvertiseTable adTables : advertisementTables) {
+                    String tableMd5 = adTables.md5;
+                    if (!adMaps.containsKey(tableMd5)) {
+                        // 接口返回数据没有,数据库有,需要删除
+                        File file = new File(
+                                getFilesDir() + "/" + AdvertiseManager.AD_DIR + "/" +
+                                        FileUtils.getFileByUrl(adTables.media_url)
+                        );
+                        if (file.exists() && file.delete()) {
+                            new Delete().from(AdvertiseTable.class)
+                                    .where(AdvertiseTable.MD5 + " = ", tableMd5)
+                                    .execute();
+                        }
+
+                    } else {
+                        // 接口返回数据和数据库都有,无需下载
+                        adMaps.remove(tableMd5);
+                    }
+                }
+                for (Map.Entry<String, AdElementEntity> entry : adMaps.entrySet()) {
+                    needDownloadAds.add(entry.getValue());
+                }
             }
-            SPUtils.putValue(AdvertiseManager.EXTRA_LAUNCH_APP_AD, shared);
-            AdvertiseManager advertiseManager = new AdvertiseManager(getApplicationContext());
-            advertiseManager.updateAppLaunchAdvertisement(adList);
+            if (!needDownloadAds.isEmpty()) {
+                AdvertiseManager advertiseManager = new AdvertiseManager(getApplicationContext());
+                advertiseManager.updateAppLaunchAdvertisement(needDownloadAds);
+            }
+
         }
 
     }
