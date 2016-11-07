77a1d09e7f31ed4c0f2d0b5a685af397318bb379
fenghuibin <fenghuibin@ismartv.cn>
2016-11-07 14:59:39 +0800

    修改个人中心-客服中心-坐标

--- usercenter/src/main/java/tv/ismar/usercenter/view/UserCenterActivity.java
+++ usercenter/src/main/java/tv/ismar/usercenter/view/UserCenterActivity.java
@@ -65,7 +65,7 @@ public class UserCenterActivity extends BaseActivity implements LoginFragment.Lo
         initViews();
 //        selectProduct();
 
-        selectLogin();
+        selectHelp();
         // Load previously saved state, if available.
         if (savedInstanceState != null) {
 //            TasksFilterType currentFiltering =
