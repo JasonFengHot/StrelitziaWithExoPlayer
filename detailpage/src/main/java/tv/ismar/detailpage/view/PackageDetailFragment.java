210949bc5708d34d404c49577a97e34d5d184217
fenghuibin <fenghuibin@ismartv.cn>
2016-11-14 16:00:24 +0800

    test

---
+++ detailpage/src/main/java/tv/ismar/detailpage/view/PackageDetailFragment.java
@@ -0,0 +1,11 @@
+package tv.ismar.detailpage.view;
+
+import tv.ismar.app.BaseFragment;
+
+/**
+ * Created by huibin on 11/14/16.
+ */
+
+public class PackageDetailFragment extends BaseFragment {
+
+}
