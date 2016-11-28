828dfd7fe67258ded977d7ec39f2659778c08e37
koujia <18701098956@139.com>
2016-11-28 17:31:57 +0800

    添加文字搜索的日志上报

--- appbase/src/main/java/tv/ismar/app/util/DeviceUtils.java
+++ appbase/src/main/java/tv/ismar/app/util/DeviceUtils.java
@@ -10,6 +10,11 @@ import android.util.TypedValue;
 import android.view.Display;
 import android.view.WindowManager;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 /**
  * Created by longhai on 16-9-12.
  */
@@ -61,4 +66,49 @@ public class DeviceUtils {
         return (int) px;
     }
 
+    public static String getLocalMacAddress(Context context) {
+        String strMacAddr = null;
+        try {
+            InetAddress ip = getLocalInetAddress();
+
+            byte[] b = NetworkInterface.getByInetAddress(ip).getHardwareAddress();
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < b.length; i++) {
+                if (i != 0) {
+                    buffer.append('-');
+                }
+                String str = Integer.toHexString(b[i] & 0xFF);
+                buffer.append(str.length() == 1 ? 0 + str : str);
+            }
+            strMacAddr = buffer.toString().toUpperCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return strMacAddr;
+    }
+
+    private static InetAddress getLocalInetAddress() {
+        InetAddress ip = null;
+        try {
+            Enumeration<NetworkInterface> en_netInterface = NetworkInterface.getNetworkInterfaces();
+            while (en_netInterface.hasMoreElements()) {
+                NetworkInterface ni = (NetworkInterface) en_netInterface.nextElement();
+                Enumeration<InetAddress> en_ip = ni.getInetAddresses();
+                while (en_ip.hasMoreElements()) {
+                    ip = en_ip.nextElement();
+                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1)
+                        break;
+                    else
+                        ip = null;
+                }
+                if (ip != null) {
+                    break;
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
 }
