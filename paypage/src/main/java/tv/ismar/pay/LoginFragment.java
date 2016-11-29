aabf686bd79415bdd0cde4336da908d54de82f7c
fenghuibin <fenghuibin@ismartv.cn>
2016-11-29 17:40:05 +0800

    Fix 2132 [夏普]从详情页进入登录页面，页面无默认焦点

--- paypage/src/main/java/tv/ismar/pay/LoginFragment.java
+++ paypage/src/main/java/tv/ismar/pay/LoginFragment.java
@@ -82,6 +82,8 @@ public class LoginFragment extends BaseFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView();
+        edit_mobile.requestFocus();
+
     }
 
     @Override
