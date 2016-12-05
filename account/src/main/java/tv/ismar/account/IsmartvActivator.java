e350e367cdb9ca1556420ae88492afabed1986d2
longhai <longhai@ismartv.cn>
2016-12-05 13:54:09 +0800

    SKY_HOST

--- account/src/main/java/tv/ismar/account/IsmartvActivator.java
+++ account/src/main/java/tv/ismar/account/IsmartvActivator.java
@@ -117,7 +117,7 @@ public class IsmartvActivator {
 
         SKY_Retrofit = new Retrofit.Builder()
                 .client(client)
-                .baseUrl(SKY_HOST_TEST)
+                .baseUrl(SKY_HOST)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
