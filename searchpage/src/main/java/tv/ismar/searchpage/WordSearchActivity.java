9f5a1504b3e13dfeb5a62992fbf872199288c72d
koujia <18701098956@139.com>
2016-12-06 17:06:38 +0800

    fixbug 2242 [夏普][585]输入框可以输入27个字符，区域内最多可显示的字母小于17

--- searchpage/src/main/java/tv/ismar/searchpage/WordSearchActivity.java
+++ searchpage/src/main/java/tv/ismar/searchpage/WordSearchActivity.java
@@ -392,7 +392,7 @@ public class WordSearchActivity extends BaseActivity implements View.OnClickList
                     showPopupWindow(t9KeyboardAdapter, view, position);
                 } else {
                     TextView tv_num = (TextView) view.findViewById(R.id.tv_num);
-                    if (et_input.getText().toString().length() <= 26) {
+                    if (et_input.getText().toString().length() <= 25) {
                         et_input.append(tv_num.getText().toString());
                     } else {
                         mediaPlayer.start();
@@ -415,7 +415,7 @@ public class WordSearchActivity extends BaseActivity implements View.OnClickList
                 TextView textView = (TextView) view.findViewById(R.id.btn_key);
                 String keyValue = textView.getText().toString();
 
-                if (et_input.getText().toString().length() <= 26) {
+                if (et_input.getText().toString().length() <= 25) {
                     et_input.append(keyValue);
                 } else {
                     mediaPlayer.start();
@@ -816,14 +816,14 @@ public class WordSearchActivity extends BaseActivity implements View.OnClickList
             }
 
         } else if (i1 == R.id.tv_key_0) {
-            if (et_input.getText().toString().length() <= 26) {
+            if (et_input.getText().toString().length() <= 25) {
                 et_input.append("0");
             } else {
                 mediaPlayer.start();
             }
 
         } else if (i1 == R.id.t9_key_0) {
-            if (et_input.getText().toString().length() <= 26) {
+            if (et_input.getText().toString().length() <= 25) {
                 et_input.append("0");
             } else {
                 mediaPlayer.start();
@@ -920,7 +920,7 @@ public class WordSearchActivity extends BaseActivity implements View.OnClickList
                 iv_left_arrow.setVisibility(View.VISIBLE);
             }
         } else {
-            if (et_input.getText().toString().length() <= 26) {
+            if (et_input.getText().toString().length() <= 25) {
                 et_input.append(((TextView) view).getText().toString());
             } else {
                 mediaPlayer.start();
