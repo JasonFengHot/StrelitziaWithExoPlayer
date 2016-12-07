d46612152bc48bef65e4c5cfd62dca8c82ae1c1e
longhai <longhai@ismartv.cn>
2016-12-07 17:56:47 +0800

    开机广告问题

--- appbase/src/main/java/tv/ismar/app/db/AdvertiseTable.java
+++ appbase/src/main/java/tv/ismar/app/db/AdvertiseTable.java
@@ -14,8 +14,6 @@ public class AdvertiseTable extends Model {
     public static final String TITLE = "title";
     public static final String START_DATE = "start_date";
     public static final String END_DATE = "end_date";
-    public static final String EVERYDAY_TIME_FROM = "everyday_time_from";
-    public static final String EVERYDAY_TIME_TO = "everyday_time_to";
     public static final String MEDIA_URL = "media_url";
     public static final String MEDIA_TYPE = "media_type";
     public static final String DURATION = "duration";
@@ -32,12 +30,6 @@ public class AdvertiseTable extends Model {
 
     @Column
     public long end_date;
-
-    @Column
-    public long everyday_time_from;
-
-    @Column
-    public long everyday_time_to;
 
     @Column
     public String media_id;
