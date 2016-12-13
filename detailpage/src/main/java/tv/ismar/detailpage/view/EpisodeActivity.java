bd23315491e82eda8138b91c87f1916ded06ebc6
koujia <18701098956@139.com>
2016-12-13 15:50:36 +0800

    fixbug 2291 【夏普】UI 电视剧剧集列表页模板没有替换,小剧集框与海报图竖向没有对齐

--- detailpage/src/main/java/tv/ismar/detailpage/view/EpisodeActivity.java
+++ detailpage/src/main/java/tv/ismar/detailpage/view/EpisodeActivity.java
@@ -3,12 +3,15 @@ package tv.ismar.detailpage.view;
 import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.IntegerRes;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,6 +30,7 @@ import tv.ismar.app.core.Source;
 import tv.ismar.app.network.entity.ItemEntity;
 import tv.ismar.app.ui.HeadFragment;
 import tv.ismar.app.widget.LoadingDialog;
+import tv.ismar.app.widget.ZGridView;
 import tv.ismar.detailpage.R;
 import tv.ismar.statistics.EpisodePageStatistics;
 
@@ -37,7 +41,7 @@ import static tv.ismar.app.core.PageIntentInterface.EXTRA_SOURCE;
  * Created by huibin on 11/29/16.
  */
 
-public class EpisodeActivity extends BaseActivity {
+public class EpisodeActivity extends BaseActivity implements View.OnHoverListener {
     private ItemEntity mItemEntity;
     private List<ItemEntity> mItemEntityList;
 
@@ -63,6 +67,7 @@ public class EpisodeActivity extends BaseActivity {
     private EpisodePageStatistics mPageStatistics;
     private Button episode_arrow_up;
     private Button episode_arrow_down;
+    private ZGridView episode_zgridview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,30 +104,33 @@ public class EpisodeActivity extends BaseActivity {
 
         initLayout();
 
-        int horizontal = getResources().getDimensionPixelOffset(R.dimen.episode_item_horizontal_space);
-        int vertical = getResources().getDimensionPixelOffset(R.dimen.episode_item_vertical_space);
-        mEpisodeRecyclerView.setSelectedItemAtCentered(false);
-        mEpisodeRecyclerView.addItemDecoration(new SpacesItemDecoration(horizontal, vertical));
-        mEpisodeRecyclerView.setLayoutManager(new GridLayoutManager(this, 10));
-        mEpisodeRecyclerView.setAdapter(new EpisodeAdapter(this, mItemEntity.getSubitems()));
-        if(mItemEntity.getSubitems().length>40){
-            episode_arrow_down.setVisibility(View.VISIBLE);
-        }
-        mEpisodeRecyclerView.setOnChildViewHolderSelectedListener(new RecyclerViewTV.OnChildViewHolderSelectedListener() {
-            @Override
-            public void onChildViewHolderSelected(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, int i) {
-                if(mEpisodeRecyclerView.getFirstVisiblePosition()>0){
-                    episode_arrow_up.setVisibility(View.VISIBLE);
-                }else{
-                    episode_arrow_up.setVisibility(View.INVISIBLE);
-                }
-                if(mEpisodeRecyclerView.getLastVisiblePosition()<mItemEntity.getSubitems().length-1){
-                    episode_arrow_down.setVisibility(View.VISIBLE);
-                }else{
-                    episode_arrow_down.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
+//        int horizontal = getResources().getDimensionPixelOffset(R.dimen.episode_item_horizontal_space);
+//        int vertical = getResources().getDimensionPixelOffset(R.dimen.episode_item_vertical_space);
+//        mEpisodeRecyclerView.setSelectedItemAtCentered(false);
+//        mEpisodeRecyclerView.addItemDecoration(new SpacesItemDecoration(horizontal, vertical));
+//        mEpisodeRecyclerView.setLayoutManager(new GridLayoutManager(this, 10));
+//        mEpisodeRecyclerView.setAdapter(new EpisodeAdapter(this, mItemEntity.getSubitems()));
+        episode_zgridview.setAdapter(new EpisodeAdapter(this,mItemEntity.getSubitems()));
+        episode_zgridview.setUpView(episode_arrow_up);
+        episode_zgridview.setDownView(episode_arrow_down);
+//        if(mItemEntity.getSubitems().length>40){
+//            episode_arrow_down.setVisibility(View.VISIBLE);
+//        }
+//        mEpisodeRecyclerView.setOnChildViewHolderSelectedListener(new RecyclerViewTV.OnChildViewHolderSelectedListener() {
+//            @Override
+//            public void onChildViewHolderSelected(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, int i) {
+//                if(mEpisodeRecyclerView.getFirstVisiblePosition()>0){
+//                    episode_arrow_up.setVisibility(View.VISIBLE);
+//                }else{
+//                    episode_arrow_up.setVisibility(View.INVISIBLE);
+//                }
+//                if(mEpisodeRecyclerView.getLastVisiblePosition()<mItemEntity.getSubitems().length-1){
+//                    episode_arrow_down.setVisibility(View.VISIBLE);
+//                }else{
+//                    episode_arrow_down.setVisibility(View.INVISIBLE);
+//                }
+//            }
+//        });
 
     }
 
@@ -137,7 +145,8 @@ public class EpisodeActivity extends BaseActivity {
 
     private void initViews() {
         tmp = findViewById(R.id.tmp);
-        mEpisodeRecyclerView = findView(R.id.episode_recycler_view);
+//        mEpisodeRecyclerView = findView(R.id.episode_recycler_view);
+        episode_zgridview = (ZGridView) findViewById(R.id.episode_zgridview);
         mImageBackground = (ImageView) findViewById(R.id.image_daram_back);
         mDramaImageLabel = (ImageView) findViewById(R.id.image_daram_label);
         mTvDramaName = (TextView) findViewById(R.id.tv_drama_name);
@@ -146,20 +155,26 @@ public class EpisodeActivity extends BaseActivity {
         one_drama_order_info = (TextView) findViewById(R.id.one_drama_order_info);
         episode_arrow_up = (Button) findViewById(R.id.episode_arrow_up);
         episode_arrow_down = (Button) findViewById(R.id.episode_arrow_down);
+        episode_arrow_up.setOnHoverListener(this);
+        episode_arrow_down.setOnHoverListener(this);
         episode_arrow_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("pos",mEpisodeRecyclerView.getSelectPostion()+"");
-                int pos=mEpisodeRecyclerView.getSelectPostion()-10<0?0:mEpisodeRecyclerView.getSelectPostion()-10;
-                mEpisodeRecyclerView.scrollToPosition(pos);
+                episode_zgridview.arrowScroll(View.FOCUS_UP);
             }
         });
         episode_arrow_down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("pos",mEpisodeRecyclerView.getSelectPostion()+"");
-                int pos=mEpisodeRecyclerView.getSelectPostion()+10>=mItemEntity.getSubitems().length?mItemEntity.getSubitems().length-1:mEpisodeRecyclerView.getSelectPostion()+10;
-                mEpisodeRecyclerView.scrollToPosition(pos);            }
+                episode_zgridview.arrowScroll(View.FOCUS_DOWN);            }
+        });
+        episode_zgridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ItemEntity subItemEntity = mItemEntity.getSubitems()[position];
+                PageIntent pageIntent = new PageIntent();
+                pageIntent.toPlayPage(EpisodeActivity.this, mItemEntity.getPk(), subItemEntity.getPk(), Source.LIST);
+            }
         });
     }
 
@@ -214,70 +229,86 @@ public class EpisodeActivity extends BaseActivity {
         super.onPause();
     }
 
-
-    private class EpisodeAdapter extends RecyclerView.Adapter<EpisodeViewHolder> implements
-            View.OnHoverListener, View.OnFocusChangeListener, View.OnClickListener {
-        private Context mContext;
-        private ItemEntity[] mItemEntities;
-
-        public EpisodeAdapter(Context context, ItemEntity[] itemEntities) {
-            mContext = context;
-            mItemEntities = itemEntities;
+    @Override
+    public boolean onHover(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_HOVER_ENTER:
+            case MotionEvent.ACTION_HOVER_MOVE:
+                v.requestFocus();
+                v.requestFocusFromTouch();
+                break;
+            case MotionEvent.ACTION_HOVER_EXIT:
+                tmp.requestFocus();
+                tmp.requestFocusFromTouch();
+                break;
         }
-
-        @Override
-        public EpisodeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(mContext).inflate(R.layout.episode_recycler_item, parent, false);
-            EpisodeViewHolder viewHolder = new EpisodeViewHolder(itemView);
-            viewHolder.episodeBtn.setOnClickListener(this);
-            viewHolder.episodeBtn.setOnHoverListener(this);
-            return viewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(EpisodeViewHolder holder, int position) {
-            ItemEntity itemEntity = mItemEntities[position];
-            Button episodeBtn = holder.episodeBtn;
-            episodeBtn.setTag(itemEntity);
-            episodeBtn.setText(String.valueOf(position + 1));
-
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mItemEntities.length;
-
-        }
-
-        @Override
-        public boolean onHover(View v, MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_HOVER_ENTER:
-                case MotionEvent.ACTION_HOVER_MOVE:
-                    v.requestFocus();
-                    v.requestFocusFromTouch();
-                    break;
-                case MotionEvent.ACTION_HOVER_EXIT:
-                    tmp.requestFocus();
-                    tmp.requestFocusFromTouch();
-                    break;
-            }
-            return true;
-        }
-
-        @Override
-        public void onFocusChange(View v, boolean hasFocus) {
-
-        }
-
-        @Override
-        public void onClick(View v) {
-            ItemEntity subItemEntity = (ItemEntity) v.getTag();
-            PageIntent pageIntent = new PageIntent();
-            pageIntent.toPlayPage(mContext, mItemEntity.getPk(), subItemEntity.getPk(), Source.LIST);
-        }
+        return true;
     }
+
+
+//    private class EpisodeAdapter extends RecyclerView.Adapter<EpisodeViewHolder> implements
+//            View.OnHoverListener, View.OnFocusChangeListener, View.OnClickListener {
+//        private Context mContext;
+//        private ItemEntity[] mItemEntities;
+//
+//        public EpisodeAdapter(Context context, ItemEntity[] itemEntities) {
+//            mContext = context;
+//            mItemEntities = itemEntities;
+//        }
+//
+//        @Override
+//        public EpisodeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            View itemView = LayoutInflater.from(mContext).inflate(R.layout.episode_recycler_item, parent, false);
+//            EpisodeViewHolder viewHolder = new EpisodeViewHolder(itemView);
+//            viewHolder.episodeBtn.setOnClickListener(this);
+//            viewHolder.episodeBtn.setOnHoverListener(this);
+//            return viewHolder;
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(EpisodeViewHolder holder, int position) {
+//            ItemEntity itemEntity = mItemEntities[position];
+//            Button episodeBtn = holder.episodeBtn;
+//            episodeBtn.setTag(itemEntity);
+//            episodeBtn.setText(String.valueOf(position + 1));
+//
+//
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return mItemEntities.length;
+//
+//        }
+//
+//        @Override
+//        public boolean onHover(View v, MotionEvent event) {
+//            switch (event.getAction()) {
+//                case MotionEvent.ACTION_HOVER_ENTER:
+//                case MotionEvent.ACTION_HOVER_MOVE:
+//                    v.requestFocus();
+//                    v.requestFocusFromTouch();
+//                    break;
+//                case MotionEvent.ACTION_HOVER_EXIT:
+//                    tmp.requestFocus();
+//                    tmp.requestFocusFromTouch();
+//                    break;
+//            }
+//            return true;
+//        }
+//
+//        @Override
+//        public void onFocusChange(View v, boolean hasFocus) {
+//
+//        }
+//
+//        @Override
+//        public void onClick(View v) {
+//            ItemEntity subItemEntity = (ItemEntity) v.getTag();
+//            PageIntent pageIntent = new PageIntent();
+//            pageIntent.toPlayPage(mContext, mItemEntity.getPk(), subItemEntity.getPk(), Source.LIST);
+//        }
+//    }
 
     private class EpisodeViewHolder extends RecyclerView.ViewHolder {
         private View mItemView;
@@ -306,5 +337,62 @@ public class EpisodeActivity extends BaseActivity {
             outRect.left = horizontal;
             outRect.right = horizontal;
         }
+
     }
+    private class EpisodeAdapter extends BaseAdapter implements  View.OnClickListener {
+
+        private Context mContext;
+        private ItemEntity[] mItemEntities;
+
+        public EpisodeAdapter(Context context, ItemEntity[] itemEntities) {
+            mContext = context;
+            mItemEntities = itemEntities;
+        }
+        @Override
+        public int getCount() {
+            return mItemEntities.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItemEntities[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder=null;
+            if(convertView==null){
+                convertView=View.inflate(mContext,R.layout.episode_recycler_item,null);
+                viewHolder=new ViewHolder();
+                viewHolder.episodeBtn= (Button) convertView.findViewById(R.id.btn_count);
+                viewHolder.episodeBtn.setTag(position);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder= (ViewHolder) convertView.getTag();
+            }
+            viewHolder.episodeBtn.setOnClickListener(this);
+            viewHolder.episodeBtn.setText(String.valueOf(position + 1));
+            return convertView;
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            ItemEntity subItemEntity = mItemEntities[(int) v.getTag()];
+            PageIntent pageIntent = new PageIntent();
+            pageIntent.toPlayPage(EpisodeActivity.this, mItemEntity.getPk(), subItemEntity.getPk(), Source.LIST);
+        }
+
+        public class ViewHolder{
+
+            public Button episodeBtn;
+        }
+
+    }
+
 }
