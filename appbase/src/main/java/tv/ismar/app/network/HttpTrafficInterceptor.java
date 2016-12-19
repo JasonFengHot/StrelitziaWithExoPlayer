56cc9973b872f33ead83b1641e7b5af6a707adc2
fenghuibin <fenghuibin@ismartv.cn>
2016-12-19 11:45:29 +0800

    revert Fix 2355 [夏普]限速，进入客户端首页然后退出再进入，高概率出现crash现象

--- appbase/src/main/java/tv/ismar/app/network/HttpTrafficInterceptor.java
+++ appbase/src/main/java/tv/ismar/app/network/HttpTrafficInterceptor.java
@@ -1,13 +1,14 @@
 package tv.ismar.app.network;
 
 import android.content.Context;
-import android.util.Log;
 
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.Protocol;
 import okhttp3.Request;
 import okhttp3.Response;
+import tv.ismar.app.VodApplication;
 import tv.ismar.app.network.exception.OnlyMobileException;
 import tv.ismar.app.network.exception.OnlyWifiException;
 import tv.ismar.app.util.NetworkUtils;
@@ -16,7 +17,6 @@ import tv.ismar.app.util.NetworkUtils;
  * Created by huibin on 8/19/16.
  */
 public class HttpTrafficInterceptor implements Interceptor {
-    private static final String TAG = "HttpTrafficInterceptor";
     private TrafficType mTrafficType = TrafficType.UNLIMITED;
     private Context mContext;
 
@@ -25,34 +25,30 @@ public class HttpTrafficInterceptor implements Interceptor {
     }
 
     @Override
-    public Response intercept(Chain chain) {
+    public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
-        Response response = null;
-        try {
-            switch (mTrafficType) {
-                case UNLIMITED:
+        Response response;
+        switch (mTrafficType) {
+            case UNLIMITED:
+                response = chain.proceed(request);
+                break;
+            case ONLY_WIFI:
+                if (NetworkUtils.isWifi(mContext)) {
                     response = chain.proceed(request);
-                    break;
-                case ONLY_WIFI:
-                    if (NetworkUtils.isWifi(mContext)) {
-                        response = chain.proceed(request);
-                    } else {
-                        throw new OnlyWifiException(request.url().toString());
-                    }
-                    break;
-                case ONLY_MOBILE:
-                    if (NetworkUtils.isMobile(mContext)) {
-                        response = chain.proceed(request);
-                    } else {
-                        throw new OnlyMobileException(request.url().toString());
-                    }
-                    break;
-                default:
+                } else {
+                    throw new OnlyWifiException(request.url().toString());
+                }
+                break;
+            case ONLY_MOBILE:
+                if (NetworkUtils.isMobile(mContext)) {
                     response = chain.proceed(request);
-                    break;
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "IOException: " + e.getMessage());
+                } else {
+                    throw new OnlyMobileException(request.url().toString());
+                }
+                break;
+            default:
+                response = chain.proceed(request);
+                break;
         }
         return response;
     }
