05e76d0dea5eed2f99634e00a8d337ac364b65e0
joyopeng <joyopeng@gmail.com>
2016-12-21 11:39:09 +0800

    fix update md5 oom.

--- account/src/main/java/tv/ismar/account/core/Md5.java
+++ account/src/main/java/tv/ismar/account/core/Md5.java
@@ -1,8 +1,13 @@
 package tv.ismar.account.core;
 
+import android.util.Log;
+
 import com.blankj.utilcode.utils.EncryptUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 /**
  * Created by huibin on 6/2/16.
@@ -12,7 +17,37 @@ public class Md5 {
         return EncryptUtils.encryptMD5ToString(string).toLowerCase();
     }
 
+//    public static String md5File(File file) {
+//       return EncryptUtils.encryptMD5File2String(file).toLowerCase();
+//    }
+
     public static String md5File(File file) {
-       return EncryptUtils.encryptMD5File2String(file).toLowerCase();
+        String value;
+        FileInputStream in;
+        try {
+            in = new FileInputStream(file);
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] buffer = new byte[1024 * 1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                messageDigest.update(buffer, 0, length);
+            }
+            BigInteger bi = new BigInteger(1, messageDigest.digest());
+            value = bi.toString(16);
+            in.close();
+        } catch (Exception e) {
+            Log.e("getMd5ByFile", e.getMessage());
+            return "";
+        }
+
+        int offset = 32 - value.length();
+        if (offset > 0) {
+            String data = new String();
+            for (int i = 0; i < offset; i++) {
+                data = data + "0";
+            }
+            value = data + value;
+        }
+        return value;
     }
 }
